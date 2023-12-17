4078fa07db9123a10bfdce7a802bf7fc5bdb9b60
amirhoseinheidary <amirhoseinheydari22@gmail.com>
2023-12-17 14:02:11 +0330

    Add user store with pinia

---
+++ src/stores/useUserStore.js
@@ -0,0 +1,27 @@
+import api from "@/api";
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+
+export const useUserStore = defineStore("user", () => {
+  const userInfo = ref(null);
+  const isLoggedIn = computed(() => userInfo.value !== null);
+  const updateUserInfo = (user) => {
+    userInfo.value = user;
+    localStorage.setItem("arvan-token", user.token);
+  };
+  const handleAuthAction = async (type, formStore) => {
+    const { user } =
+      type === "register"
+        ? await api.register(formStore)
+        : await api.login(formStore);
+
+    updateUserInfo(user);
+  };
+  const getUserInfo = async () => {
+    const { user } = await api.getUserInfo();
+
+    updateUserInfo(user);
+  };
+
+  return { userInfo, isLoggedIn, handleAuthAction, getUserInfo };
+});
